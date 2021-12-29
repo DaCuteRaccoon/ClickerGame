f80be4743db429a93f273707ba9237db1a22a1a9
DaCuteRaccoon <72467156+DaCuteRaccoon@users.noreply.github.com>
2021-12-28 19:02:34 -0800

    Create christmas.js

---
+++ src/js/christmas.js
@@ -0,0 +1,28 @@
+//            /\      
+//           /\*\     
+//          /\O\*\    
+//         /*/\/\/\   
+//        /\O\/\*\/\  
+//       /\*\/\*\/\/\ 
+//      /\O\/\/*/\/O/\
+//            ||      
+//            ||      
+//            ||      
+
+// You found the tree! (It says to find it in the changelog)
+
+(async function() {
+  console.log(`        ✯      \n` + `%c        /\\      
+       /\\*\\     
+      /\\O\\*\\    
+     /*/\\/\\/\\   
+    /\\O\\/\\*\\/\\  
+   /\\*\\/\\*\\/\\/\\ 
+  /\\O\\/\\/*/\\/O/\\
+        ||      
+        ||      
+        ||`, "font-family:monospace");
+})();
+
+// Find more stars to decorate your lovely tree here:
+// https://fsymbols.com/signs/stars/
